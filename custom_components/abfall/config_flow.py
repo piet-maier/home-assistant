ce0b63176ffee50bda03e53af7a73b5a379f5609
Piet Maier <pietmaier.github@protonmail.com>
2025-09-01 19:25:25 +0200

    FEAT(Abfall): Add Config Flow

---
+++ custom_components/abfall/config_flow.py
@@ -0,0 +1,20 @@
+import typing
+
+import voluptuous
+from homeassistant.config_entries import ConfigFlow
+
+from . import DOMAIN
+
+
+class AbfallConfigFlow(ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    MINOR_VERSION = 1
+
+    async def async_step_user(self, user_input: dict[str, typing.Any] | None = None):
+        if user_input is not None:
+            return self.async_create_entry(title="Abfall", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=voluptuous.Schema({voluptuous.Required("street"): int}),
+        )
