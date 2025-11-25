d23d0dbdfc9c3cf94458b4e7b9f370370ba67a57
Piet Maier <pietmaier.github@protonmail.com>
2025-11-25 16:23:10 +0100

    FEAT(Thermostat): Add Config Flow

---
+++ custom_components/thermostat/config_flow.py
@@ -0,0 +1,34 @@
+import typing
+
+import voluptuous
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.const import CONF_ENTITIES, CONF_NAME, Platform
+from homeassistant.helpers.selector import (
+    EntitySelector,
+    EntitySelectorConfig,
+)
+
+from . import DOMAIN
+
+
+class ThermostatConfigFlow(ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+    MINOR_VERSION = 1
+
+    async def async_step_user(self, user_input: dict[str, typing.Any] | None = None):
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=voluptuous.Schema(
+                {
+                    voluptuous.Required(CONF_NAME): str,
+                    voluptuous.Required(CONF_ENTITIES): EntitySelector(
+                        EntitySelectorConfig(
+                            domain=Platform.CLIMATE, multiple=True, reorder=True
+                        )
+                    ),
+                }
+            ),
+        )
