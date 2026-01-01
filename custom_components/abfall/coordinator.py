9bb33f2bfef8d203e0bdbd6aee0c54e1c4d3b988
Piet Maier <pietmaier.github@protonmail.com>
2026-01-01 17:19:20 +0100

    REFACTOR(Abfall): Add RSAG Client

--- custom_components/abfall/coordinator.py
+++ custom_components/abfall/coordinator.py
@@ -4,16 +4,16 @@ import logging
 from homeassistant.components.calendar import CalendarEvent
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
+from .client.rsag import RSAG
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
 class AbfallCoordinator(DataUpdateCoordinator[list[CalendarEvent]]):
-    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry):
+    def __init__(self, hass: HomeAssistant, config_entry: ConfigEntry, client: RSAG):
         super().__init__(
             hass,
             _LOGGER,
@@ -23,32 +23,10 @@ class AbfallCoordinator(DataUpdateCoordinator[list[CalendarEvent]]):
             always_update=False,
         )
 
+        self.client = client
+
     async def _async_update_data(self):
         if self.config_entry is None:
             raise TypeError("The configuration entry must not be None.")
 
-        client = async_get_clientsession(self.hass)
-
-        async with client.get(
-            "https://www.rsag.de/api/pickup/filter/"
-            + str(self.config_entry.data["street"])
-            + "/1,2,3,4,6,7/"
-            + str(datetime.datetime.now().month)
-        ) as response:
-            data = await response.json(content_type=None)
-
-            result: list[CalendarEvent] = []
-
-            if not data:
-                return result
-
-            for item in data[0]["items"]:
-                date = datetime.date.fromisoformat(item["pickupdate"])
-
-                result.append(
-                    CalendarEvent(
-                        date, date + datetime.timedelta(days=1), item["wastetype_name"]
-                    )
-                )
-
-            return sorted(result, key=lambda item: item.start)
+        return await self.client.async_fetch_data(self.config_entry.data["street"])
