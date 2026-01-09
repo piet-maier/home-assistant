0badeb0b03ed874af8998094ed846ed09f28e334
Piet Maier <pietmaier.github@protonmail.com>
2026-01-09 16:50:40 +0100

    FEAT(Thermostat): Add Temperature Sensor

--- custom_components/thermostat/climate.py
+++ custom_components/thermostat/climate.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Any
 
 from homeassistant.components.climate import ClimateEntity
@@ -31,15 +32,27 @@ from homeassistant.const import (
     STATE_UNAVAILABLE,
     STATE_UNKNOWN,
 )
-from homeassistant.core import HomeAssistant, callback
+from homeassistant.core import Event, EventStateChangedData, HomeAssistant, callback
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.event import async_track_state_change_event
+
+from .const import SENSOR
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
 ):
     async_add_entities(
-        [ThermostatEntity(hass, entry.data[CONF_NAME], entry.data[CONF_ENTITIES])]
+        [
+            ThermostatEntity(
+                hass,
+                entry.data[CONF_NAME],
+                entry.data[CONF_ENTITIES],
+                entry.data.get(SENSOR),
+            )
+        ]
     )
 
 
@@ -54,7 +67,9 @@ class ThermostatEntity(GroupEntity, ClimateEntity):
 
     _attr_hvac_modes = [HVACMode.OFF, HVACMode.HEAT]
 
-    def __init__(self, hass: HomeAssistant, name: str, entities: list[str]):
+    def __init__(
+        self, hass: HomeAssistant, name: str, entities: list[str], sensor: str | None
+    ):
         self.hass = hass
 
         self._attr_name = name
@@ -62,6 +77,41 @@ class ThermostatEntity(GroupEntity, ClimateEntity):
         self._entity_ids = entities
 
         self._attr_temperature_unit = self.hass.config.units.temperature_unit
+
+        self._sensor_id = sensor
+
+    async def async_added_to_hass(self):
+        await super().async_added_to_hass()
+
+        if self._sensor_id is None:
+            return
+
+        self.async_on_remove(
+            async_track_state_change_event(
+                self.hass, self._sensor_id, self._async_temperature_sensor_state_change
+            )
+        )
+
+    @callback
+    async def _async_temperature_sensor_state_change(
+        self, state_change: Event[EventStateChangedData]
+    ):
+        if state_change.data["new_state"] is None:
+            return
+
+        sensor_state = state_change.data["new_state"].state
+
+        if sensor_state in [STATE_UNAVAILABLE, STATE_UNKNOWN]:
+            return
+
+        number_state = float(sensor_state)
+
+        _LOGGER.info(
+            'Setting current temperature of "%s" to %.1f %s...',
+            self._attr_name,
+            number_state,
+            self._attr_temperature_unit,
+        )
 
     @callback
     def async_update_group_state(self):
--- custom_components/thermostat/config_flow.py
+++ custom_components/thermostat/config_flow.py
@@ -8,7 +8,7 @@ from homeassistant.helpers.selector import (
     EntitySelectorConfig,
 )
 
-from . import DOMAIN
+from .const import DOMAIN, SENSOR
 
 
 class ThermostatConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -28,6 +28,9 @@ class ThermostatConfigFlow(ConfigFlow, domain=DOMAIN):
                         EntitySelectorConfig(
                             domain=Platform.CLIMATE, multiple=True, reorder=True
                         )
+                    ),
+                    SENSOR: EntitySelector(
+                        EntitySelectorConfig(domain=Platform.SENSOR)
                     ),
                 }
             ),
