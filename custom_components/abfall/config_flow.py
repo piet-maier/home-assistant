f02af1e18aee47d73b2e1bb6fe65a6664b3ba57e
Piet Maier <pietmaier.github@protonmail.com>
2026-03-01 22:17:55 +0100

    FEAT(Abfall): Add Reconfiguration

--- custom_components/abfall/config_flow.py
+++ custom_components/abfall/config_flow.py
@@ -5,6 +5,8 @@ from homeassistant.config_entries import ConfigFlow
 
 from .const import DOMAIN
 
+SCHEMA = voluptuous.Schema({voluptuous.Required("street"): int})
+
 
 class AbfallConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -14,7 +16,14 @@ class AbfallConfigFlow(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title="Abfall", data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=voluptuous.Schema({voluptuous.Required("street"): int}),
-        )
+        return self.async_show_form(step_id="user", data_schema=SCHEMA)
+
+    async def async_step_reconfigure(
+        self, user_input: dict[str, typing.Any] | None = None
+    ):
+        if user_input is not None:
+            return self.async_update_reload_and_abort(
+                self._get_reconfigure_entry(), data_updates=user_input
+            )
+
+        return self.async_show_form(step_id="reconfigure", data_schema=SCHEMA)
